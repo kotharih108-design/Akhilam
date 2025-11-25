3dcb2c25bbf4d31b175aef2ae6402e171f489ff1
Darshil Patel <darshilpatel1413@gmail.com>
2025-11-25 13:17:02 +0530

    ds

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 export default function Navbar() {
   const [open, setOpen] = useState(false);
@@ -22,6 +22,7 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-4 py-2 flex items-center justify-between">
 
         {/* LOGO + NAME */}
+        <Link to="/" >
         <div className="flex items-center gap-3">
           <img
             src="/images/logo/akhilam-logo.png"
@@ -29,6 +30,7 @@ export default function Navbar() {
             className="h-16 w-auto"
           />
         </div>
+        </Link>
 
         {/* DESKTOP MENU */}
         <div className="hidden md:flex items-center gap-10 text-md font-semibold tracking-wide text-brandOrange font-brand ">
